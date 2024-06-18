f4c4c20b6c28292f52d4898981ff25b4dc6893cc
Umme-Zaiba <ummezaiba24j@gmail.com>
2024-06-18 17:22:03 +0530

    Update App

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,21 +1,27 @@
 // components/TaskList.js
 import React from 'react';
 import TaskItem from './TaskItem';
-import { List, Paper } from '@mui/material';
+import { List, Paper, Typography } from '@mui/material';
 
 const TaskList = ({ tasks, deleteTask, completeTask }) => {
   return (
     <Paper elevation={3} sx={{ p: 2, mb: 3 }}>
-      <List>
-        {tasks.map(task => (
-          <TaskItem
-            key={task.id}
-            task={task}
-            deleteTask={deleteTask}
-            completeTask={completeTask}
-          />
-        ))}
-      </List>
+      {tasks.length === 0 ? (
+        <Typography variant="h6" align="center" color="textSecondary">
+          No tasks available
+        </Typography>
+      ) : (
+        <List>
+          {tasks.map(task => (
+            <TaskItem
+              key={task.id}
+              task={task}
+              deleteTask={deleteTask}
+              completeTask={completeTask}
+            />
+          ))}
+        </List>
+      )}
     </Paper>
   );
 };
